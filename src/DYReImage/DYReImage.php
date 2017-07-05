03d51a152602c5bfb1726d6f2ccb5a8e436ee482
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-07-05 21:08:06 +0530

    update

--- src/DYReImage/DYReImage.php
+++ src/DYReImage/DYReImage.php
@@ -2,7 +2,7 @@
 /**
  * file: DYReImage.php
  * author: yusuf shakeel
- * github: https://github.com/yusufshakeel/dyreimage
+ * github: https://github.com/yusufshakeel/dyreimage-php
  * date: 12-feb-2014 wed
  * description: This file contains the DYReImage class.
  * 
